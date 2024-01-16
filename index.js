f239923b5f1955ff3602284ca7098cea5d5dee79
fiandev <wongkeras2@gmail.com>
2024-01-17 05:25:28 +0700

    feat: update moving file rules

--- index.js
+++ index.js
@@ -10,25 +10,32 @@ const rules = {
   "./files/archives/@": "./*.{zip,rar,tr.gz,7z,tar,tar.gz}",
   "./files/codes/@": "./*.{xml,html,css,js,jsx,tsx,ts,sql,md,json}",
   "./files/documents/@": "./*.{pdf,xlxx,docx,docs}",
+  "./files/applications/windows/@": "./*.{exe,msi}",
+  "./files/applications/android/@": "./*.apk",
+  "./files/applications/linux/debian/@": "./*.deb",
+  "./files/applications/bootables/@": "./*.iso",
+  "./files/transfers/@": "./*.torrent",
   "./others/@": "./*.{bak,txt}",
 };
 
-( async () => {
+(async () => {
   for (let key in rules) {
     let files = await glob([rules[key]]);
-    
+
     if (files.length < 1) continue;
-    
+
     for (let file of files) {
       if (!fs.lstatSync(file).isFile()) continue;
-      
+
       let { base, ext } = path.parse(file);
-      let extension = [".gz"].includes(ext) ? base.slice(base.search(/\./) + 1) : ext.slice(1);
-      let dest = key.replace(/\@/, extension) + `/${ base }`;
-      
+      let extension = [".gz"].includes(ext)
+        ? base.slice(base.search(/\./) + 1)
+        : ext.slice(1);
+      let dest = key.replace(/\@/, extension) + `/${base}`;
+
       fsx.move(file, dest, (err) => {
-        if (err) return console.log(`[${ base }]: ${ err.message }`);
-        console.log(`${ base } moved successfully`);
+        if (err) return console.log(`[${base}]: ${err.message}`);
+        console.log(`${base} moved successfully`);
       });
     }
   }
---
+++ test.md
@@ -0,0 +1,16 @@
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+
+```
+
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
